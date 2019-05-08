186f186224a4c437126c28805686fca1ded7f403
Toto Corvidoni <totocorvidoni@gmail.com>
2019-05-08 12:56:14 -0300

    Delegate cart logic to actions.

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,42 +2,53 @@ export default {
   setAllCategories(state, payload) {
     state.allCategories = payload;
   },
+
   setFeaturedProducts(state, products) {
     state.featuredProducts = products;
   },
+
   setActiveMenuCategory(state, menuCategoryId) {
     state.activeMenuCategory = menuCategoryId;
   },
+
   setActiveProduct(state, productInfo) {
     state.activeProduct = productInfo;
   },
+
   setDisplayedCategory(state, categoryId) {
     state.displayedCategory = categoryId;
   },
+
   setDisplayedProducts(state, payload) {
     state.displayedProducts = payload;
   },
+
   startLoadingProducts(state) {
     state.productsLoading = true;
   },
+
   stopLoadingProducts(state) {
     state.productsLoading = false;
   },
+
   setRelatedProducts(state, payload) {
     state.relatedProducts = payload;
   },
 
   addToCart(state, product) {
+    product.amount = 1;
     state.cart.push(product);
   },
-  removeFromCart(state, product) {
-    const index = state.cart.findIndex(item => {
-      return item.id == product.id;
-    });
-    if (index == -1) {
-      console.log("unable to find the product to delete");
-    } else {
-      state.cart.splice(index, 1);
-    }
+
+  removeFromCart(state, index) {
+    state.cart.splice(index, 1);
+  },
+
+  incrementItemInCart(state, index) {
+    state.cart[index].amount += 1;
+  },
+
+  decrementItemInCart(state, index) {
+    state.cart[index].amount -= 1;
   }
 };
