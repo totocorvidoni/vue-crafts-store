510553c97e1569b23492f5f9f166fd8e73bee86b
Toto Corvidoni <totocorvidoni@gmail.com>
2019-05-08 13:00:11 -0300

    Add Cart.

---
+++ src/components/CartItem.vue
@@ -0,0 +1,21 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'cart-item',
+  props: {
+    id: Number,
+    image: Object,
+    name: String,
+    price: String,
+    amount: Number
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+ 
---
+++ src/views/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Mi pedido</h1>
+    <div v-for="item in items" :key="item.id">
+      <cart-item
+        :id="item.id"
+        :image="item.image[0]"
+        :name="item.name"
+        :price="item.price"
+        :amount="item.amount"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import CartItem from "@/components/CartItem.vue";
+
+export default {
+  name: "cart",
+  components: {
+    CartItem
+  },
+  computed: {
+    items() {
+      return this.$store.cart;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
