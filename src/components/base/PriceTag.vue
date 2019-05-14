a84d9ac8938b2228219e9787348c585ac67ba47b
Toto Corvidoni <totocorvidoni@gmail.com>
2019-05-14 13:22:59 -0300

    Move formatPrice as a mixin.

--- src/components/base/PriceTag.vue
+++ src/components/base/PriceTag.vue
@@ -10,43 +10,15 @@
 </template>
 
 <script>
+import priceFormatter from '@/components/mixins/priceFormatter'
+
 export default {
   name: "price-tag",
   props: {
     price: String,
     priceRegular: String
   },
-
-  methods: {
-    formatPrice(amount, decimalCount = 2, decimal = ".", thousands = ",") {
-      try {
-        decimalCount = Math.abs(decimalCount);
-        decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
-
-        const negativeSign = amount < 0 ? "-" : "";
-
-        let i = parseInt(
-          (amount = Math.abs(Number(amount) || 0).toFixed(decimalCount))
-        ).toString();
-        let j = i.length > 3 ? i.length % 3 : 0;
-
-        return (
-          "$" +
-          negativeSign +
-          (j ? i.substr(0, j) + thousands : "") +
-          i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) +
-          (decimalCount
-            ? decimal +
-              Math.abs(amount - i)
-                .toFixed(decimalCount)
-                .slice(2)
-            : "")
-        );
-      } catch (e) {
-        console.log(e);
-      }
-    }
-  }
+  mixins: [priceFormatter]
 };
 </script>
 
