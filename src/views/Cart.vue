9bc6afe18fc784340a44d37d8207eef4f63a2e43
Toto Corvidoni <totocorvidoni@gmail.com>
2019-05-21 10:47:25 -0300

    Update styling.

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="cart">
-    <h1 class="title">Mi pedido</h1>
+    <div class="section-title title">
+      <h1>Lo que voy a llevar</h1>
+    </div>
     <div class="items-wrapper">
       <cart-item
         v-for="item in items"
@@ -12,7 +14,7 @@
         :image="item.images[0]"
       />
     </div>
-    <div class="details">
+    <div class="totals">
       <div class="title">
         <h2>Resumen</h2>
       </div>
@@ -29,8 +31,13 @@
           <span>
             <strong>Total:</strong>
           </span>
-          <span><strong>{{ formatPrice(priceSum) }}</strong></span>
+          <span>
+            <strong>{{ formatPrice(priceSum) }}</strong>
+          </span>
         </div>
+        <p class="note">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi optio, blanditiis voluptate temporibus tempore praesentium pariatur nostrum quos minima esse,
+        </p>
         <button class="button confirm">COMPRAR</button>
       </div>
     </div>
@@ -65,16 +72,30 @@ export default {
 <style lang="scss">
 .cart {
   display: grid;
-  grid-gap: 1rem;
-  grid-template-columns: 3fr 1fr;
+  grid-column-gap: 3rem;
+  grid-template-columns: 2fr 1fr;
   grid-template-rows: 4em 1fr;
   justify-content: center;
   align-items: start;
+  padding: 1em 2em;
 
-  .title {
-    align-self: center;
-    justify-self: center;
+  .section-title {
+    place-self: stretch;
     grid-column: 1;
+    display: grid;
+    align-content: center;
+    justify-content: start;
+    // background: $color4;
+    border-top-left-radius: 0.5em;
+    border-top-right-radius: 0.5em;
+    // font-family: $font-condensed;
+    padding: 1em;
+    color: $color1;
+    & > h1 {
+    // text-transform: uppercase;
+      font-weight: 400;
+      font-size: 1.5em;
+    }
   }
 
   .items-wrapper {
@@ -83,22 +104,22 @@ export default {
     grid-template-columns: repeat(auto-fit, 1fr);
     grid-column-gap: 3rem;
     grid-row-gap: 1.5rem;
-    padding: 0 2em;
     justify-content: center;
   }
 
-  .details {
+  .totals {
     grid-column: 2;
     grid-row: 1 / -1;
     font-family: $font-condensed;
-    margin-top: 1em;
     min-width: 250px;
 
     .title {
       background: $color4;
+      // border-top-left-radius: 0.5em;
+      // border-top-right-radius: 0.5em;
       color: $color-adaptable;
       text-transform: uppercase;
-      text-align: end;
+      text-align: start;
       width: 100%;
       padding: 0 1rem;
     }
@@ -107,6 +128,7 @@ export default {
       border: $border-card;
       border-top: none;
       border-bottom-left-radius: 0.5em;
+      border-bottom-right-radius: 0.5em;
       color: $color-dark-light;
       padding: 1rem;
     }
@@ -117,12 +139,21 @@ export default {
     justify-content: space-between;
   }
 
+  .note {
+    background: $color4-light;
+    margin: 1em;
+    border-radius: 0.5em;
+    padding: 1em;
+    color: $color-dark;
+    text-align: justify;
+  }
+
   .confirm {
     display: block;
     background: $color-good;
     border: none;
     color: $color-light;
-    padding: .2em .4em;
+    padding: 0.2em 0.4em;
     margin-top: 1em;
     margin-left: auto;
     font-size: 1em;
