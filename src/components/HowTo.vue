8ce5bb99b6b1904ecfe8fbf55bed2294ff239315
Toto Corvidoni <totocorvidoni@gmail.com>
2019-07-10 11:43:30 -0300

    Add wrap.

--- src/components/HowTo.vue
+++ src/components/HowTo.vue
@@ -3,19 +3,25 @@
     <h2 class="uppercase home-title">¿Como trabajamos?</h2>
     <div class="steps-wrapper">
       <div class="step">
-        <img src="@/assets/store-solid.svg" class="icon" />
-        <h3>Descubrí</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aut omnis officia facere natus ratione, voluptatum corporis, cum esse eum aliquam unde voluptates ea dicta soluta, quia excepturi explicabo laboriosam.</p>
+        <div class="content">
+          <img src="@/assets/store-solid.svg" class="icon" />
+          <h3>Descubrí</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aut omnis officia facere natus ratione, voluptatum corporis, cum esse eum aliquam unde voluptates ea dicta soluta, quia excepturi explicabo laboriosam.</p>
+        </div>
       </div>
       <div class="step">
-        <img src="@/assets/shopping-bag-dark.svg" class="icon" />
-        <h3>Elegí</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi tempore odio amet voluptatibus, beatae nemo est ad dolore accusantium placeat, omnis accusamus nesciunt rerum sit vitae tenetur quam labore quidem?</p>
+        <div class="content">
+          <img src="@/assets/shopping-bag-dark.svg" class="icon" />
+          <h3>Elegí</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi tempore odio amet voluptatibus, beatae nemo est ad dolore accusantium placeat, omnis accusamus nesciunt rerum sit vitae tenetur quam labore quidem?</p>
+        </div>
       </div>
       <div class="step">
-        <img src="@/assets/paper-plane-solid.svg" class="icon" />
-        <h3>Pedinos</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptate accusantium et tempore consequuntur molestias, aliquid recusandae dolorum? Doloribus rem voluptates dolore neque odit, minus minima omnis architecto nam non.</p>
+        <div class="content">
+          <img src="@/assets/paper-plane-solid.svg" class="icon" />
+          <h3>Pedinos</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptate accusantium et tempore consequuntur molestias, aliquid recusandae dolorum? Doloribus rem voluptates dolore neque odit, minus minima omnis architecto nam non.</p>
+        </div>
       </div>
     </div>
   </div>
@@ -31,20 +37,21 @@ export default {
 .how-to {
   .steps-wrapper {
     display: flex;
+    flex-wrap: wrap;
     justify-content: center;
-    background: $color4-light;
-    border-radius: 0.5em;
-    margin: 0 auto;
-    padding: 0.5em;
-    width: min-content;
   }
 
   .step {
+    background: $color4-light;
+    padding: 1em;
+  }
+
+  .content {
     border: 2px solid $color-background;
     border-radius: 0.5em;
-    margin: 0.5em;
     padding: 1em;
     width: 15em;
+    height: 100%;
 
     &:hover h3 {
       color: $color3;
